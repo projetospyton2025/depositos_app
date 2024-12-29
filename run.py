84f42c27b9a35c518c0bfec28737285e4479f06c
Marcio Maia <marciofernando.maia@yahoo.com.br>
2024-12-29 16:34:08 -0300

    Feito  alteração no arquivo run.py para rodar no Render

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-# run.py
+# run.py (DESCOMENTE PARA RODAR LOCALMENTE)
+"""
 from app import create_app, db
 
 app = create_app()
@@ -7,3 +8,19 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+"""
+# run.py (COMENTE PARA RODAR LOCALMENTE)
+from app import create_app, db
+import os
+
+app = create_app()
+
+if __name__ == '__main__':
+    # Configura o contexto do aplicativo para criar tabelas no banco de dados.
+    with app.app_context():
+        db.create_all()
+
+    # Obtém a porta do ambiente (Render) ou usa 5000 como padrão.
+    port = int(os.environ.get("PORT", 5000))
+    # Executa o servidor no modo de produção.
+    app.run(host="0.0.0.0", port=port, debug=False)
